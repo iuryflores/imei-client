1bbfe9bf89fbfb976f5387c1061f582be235fe54
iuryflores <iury.flores@gmail.com>
2023-09-15 11:12:57 -0300

    add ImeiReader

---
+++ src/components/ImeiReader.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+const ImeiReader = ({ onImeiAdd }) => {
+  const [imei, setImei] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (imei) {
+        onImeiAdd(imei);
+        setImei("");
+      }
+    }, 3000); // Aguarde 3 segundos após a última alteração
+
+    return () => clearTimeout(timer);
+  }, [imei, onImeiAdd]);
+
+  const handleImeiChange = (event) => {
+    setImei(event.target.value);
+  };
+
+  return (
+    <div className="d-flex flex-column form-group">
+      <label>Leitura de IMEI</label>
+      <input
+        className="form-control"
+        type="text"
+        value={imei}
+        onChange={handleImeiChange}
+        placeholder="Insira o IMEI"
+      />
+    </div>
+  );
+};
+
+export default ImeiReader;
